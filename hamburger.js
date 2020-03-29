c4ae977370ebedce5ae2ff0e5b3a2f923cdaeaa3
Jeremy <wu_jeremy94@outlook.com>
2020-03-29 16:27:06 +1100

    hamburger working

--- hamburger.js
+++ hamburger.js
@@ -1,8 +1,17 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+let mainNav=document.getElementById('main-nav');
+let navbarToggle=document.getElementById('navbar-toggle');
+
+
+
+navbarToggle.addEventListener('click',function(){
+
+    if(this.classList.contains('active')){
+        mainNav.style.display="none";
+        this.classList.remove('active');
+    }
+    else{
+        mainNav.style.display="flex";
+        this.classList.add('active');
+
+    }
+});
